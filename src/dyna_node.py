ebcd2cb372af788f9d7fe88e53e6dcfb4e5967d9
User1 Ros20 <user1@ros20.edu>
2018-07-15 22:29:44 -0400

    linear control logic good and usable

--- src/dyna_node.py
+++ src/dyna_node.py
@@ -4,39 +4,32 @@ import dynamixel_mx28 as dm28
 import rospy
 from geometry_msgs.msg import Vector3
 
-
 dyna = dm28.dynamixel_mx28(dxl_id=3)
 dyna.set_left_limit(3600)
 dyna.set_right_limit(1500)
 SET_POINT = 320
 
+# Each time a target centroid is published then center the camera on it.
 def callback(data):
-    centroid = data.x
     set_speed = 0
-    error = SET_POINT - centroid
-    if error > 0:
-        # move cw
-        set_speed = error
-    elif error < 0:
-        #move cw
-        set_speed = 1024 + abs(error)
-    else:
+    if data.z ==1: # For now only shoot the first target, ignore others
+        centroid = data.x
         set_speed = 0
-    print("SetSpeed: %d, Error: %d" % (set_speed,error))
+        error = SET_POINT - centroid
+        if error > 0:
+            # move cw
+            set_speed = error*0.5
+        elif error < 0:
+            #move ccw
+            set_speed = 1024 + abs(error)*0.5
+
+    #print("SetSpeed: %d, Error: %d" % (set_speed,error))
     dyna.set_moving_speed(int(set_speed))
     
     
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('face_shooter', anonymous=True)
-
     rospy.Subscriber("/face_centroid", Vector3, callback)
-
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
--- src/face_tracker_node.py
+++ src/face_tracker_node.py
@@ -3,13 +3,12 @@
 import rospy
 import cv2
 from geometry_msgs.msg import Vector3
-from sensor_msgs.msg import Image, CameraInfo
+from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import numpy as np
 
-#frame= np.zeros((256, 256, 1), dtype = "uint8")
 img = Image()
 
+# This callback updates a global variable only
 def image_callback(ros_image):
     global img
     img = ros_image
@@ -29,7 +28,15 @@ if __name__ == '__main__':
     
     # Create the classifier
     face_cascade=cv2.CascadeClassifier(haar_file_face)
-    rate = rospy.Rate(20) # 20hz
+
+    # Waiting here until first image appears
+    rate = rospy.Rate(30) # 30hz
+    while not rospy.is_shutdown(): 
+        if img.encoding != '':
+            break
+        rate.sleep()
+
+    # Process the images
     while not rospy.is_shutdown():
         #hello_str = "hello world %s" % rospy.get_time()
         #rospy.loginfo(hello_str)
@@ -37,30 +44,30 @@ if __name__ == '__main__':
             # Use cv_bridge() to convert the ROS image to OpenCV format
             bridge = CvBridge()
             frame = bridge.imgmsg_to_cv2(img, "bgr8") 
-            if (display_original_image):        
+            if (display_original_image): # Dispaly the image if param is set to true       
                 cv2.imshow('original',frame)              
             # Gray scale image            
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # Use the classifier to find faces
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            center=Vector3()
-            for index,(x,y,w,h) in enumerate(faces):
+            center=Vector3() 
+            for index,(x,y,w,h) in enumerate(faces): # For each face publish the centroid
                 #print("Frame is: %d by %d and x,y %d, %d" %(frame.shape[1],frame.shape[0],x,y))
-                frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+                frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2) # Draw a frame around each face
                 # publish center of face
                 center.x=x+w/2
                 center.y=y+h/2
                 center.z=index+1 # unique id for multiple faces, zero indexed
                 pub.publish(center)
             if center.z == 0: # No faces found so aim at center
-                center.x = 320
+                center.x = 320 # This is the set Point TODO use a param
                 pub.publish(center)
-            if (display_tracking_image):        
+            if (display_tracking_image): # Dispaly the image if param is set to true
                 cv2.imshow('tracking',frame)     
-        #except CvBridgeError, e:
         except Exception as e:
             print(e)
         
-        cv2.waitKey(1) 
+        cv2.waitKey(1) # Needed 
         rate.sleep()
     
     cv2.destroyAllWindows()
